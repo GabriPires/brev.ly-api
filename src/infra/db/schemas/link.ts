d85e39dbf0b07fa43efe15a655c2be13e8455b28
Gabriel Pires <gabpiresdev@gmail.com>
2025-04-21 09:53:04 -0300

    feat: implement postgres  database schema and initial migration for 'link' table

---
+++ src/infra/db/schemas/link.ts
@@ -0,0 +1,11 @@
+import { pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+import { uuidv7 } from 'uuidv7'
+
+export const link = pgTable('link', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => uuidv7()),
+  originalUrl: text('original_url').notNull(),
+  shortUrl: text('short_url').notNull().unique(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+})
